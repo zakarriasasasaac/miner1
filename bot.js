a51f62e0ba321bcf37a4de70e4f56a32db2e2b09
zakarriasasasaac <44728190+zakarriasasasaac@users.noreply.github.com>
2018-11-03 18:13:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,11 +4,19 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("507989504888406022")
+let channel =     client.channels.get("505848675239460866")
 setInterval(function() {
-channel.send(`salemsalemsalemsalemsalemsalemsalemsalemsalemsalemsalem`);
+channel.send(`zazazajxhsafhuishsddhksdhksdo;isjks;jskdjskgjkdlsjijhl;kj;ij;ij;kj;ijio;ji;jlk;`);
 }, 30)
 })
+client.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "1say") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
 client.on("message", function(message) {
     var args = message.content.split(/ +/g);
     var command = args.shift()
